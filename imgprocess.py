c2d7ba4232a8504ea1758819c5fdee962c171a46
Paulo Prates <paulo.prates01@hotmail.com>
2025-06-28 18:59:03 -0300

    fix: falha na separação de tons amarelos

--- imgprocess.py
+++ imgprocess.py
@@ -8,7 +8,8 @@ def getBinaryImage(img, gaussian_ksize):
   # Aplicação do filtro gaussiano
   blurred_img = cv.GaussianBlur(gray_img, gaussian_ksize, 0)
   # Binarização da imagem
-  _, binary_img = cv.threshold(blurred_img, 120, 255, cv.THRESH_BINARY_INV)
+  binary_img = cv.adaptiveThreshold(blurred_img, 255, cv.ADAPTIVE_THRESH_GAUSSIAN_C, cv.THRESH_BINARY_INV, 23, 2)
+
 
   return binary_img
 
@@ -20,37 +21,37 @@ def getDeviceContour(img):
   # Retornar o maior contorno encontrado
   if contours:
     cnt = max(contours, key=cv.contourArea)
+    cv.drawContours(img, [cnt], -1, (0, 255, 0), 2)
     x, y, w, h = cv.boundingRect(cnt)
-    return img[y:y+h, x:x+w]
+    return img[y:y+h, x:x+w], w, h
   return None
 
 def getDisplayContour(img, target_area):
   # Obter a imagem binarizada
-  binary_img = getBinaryImage(img, (5, 5))
+  binary_img = getBinaryImage(img, (3, 3))
   # Encontrar contornos na imagem binarizada
   contours, _ = cv.findContours(binary_img, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
 
   for cnt in contours:
     area = cv.contourArea(cnt)
-    if target_area * 0.7 <= area <= target_area * 1.3:
+    if target_area * 0.3 <= area <= target_area * 1.5:
       # Aproximar o contorno para reduzir o número de pontos
       epsilon = 0.02 * cv.arcLength(cnt, True)
       approx = cv.approxPolyDP(cnt, epsilon, True)
-      if len(approx) == 4:
-        if len(approx) == 4 and cv.isContourConvex(approx):
-          angles = []
-          for i in range(4):
-            p1 = approx[i][0]
-            p2 = approx[(i + 1) % 4][0]
-            p3 = approx[(i + 2) % 4][0]
+      if len(approx) == 4 and cv.isContourConvex(approx):
+        angles = []
+        for i in range(4):
+          p1 = approx[i][0]
+          p2 = approx[(i + 1) % 4][0]
+          p3 = approx[(i + 2) % 4][0]
 
-            v1 = p1 - p2
-            v2 = p3 - p2
-            cos_angle = np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
-            angle = np.degrees(np.arccos(cos_angle))
-            angles.append(angle)
+          v1 = p1 - p2
+          v2 = p3 - p2
+          cos_angle = np.dot(v1, v2) / (np.linalg.norm(v1) * np.linalg.norm(v2))
+          angle = np.degrees(np.arccos(cos_angle))
+          angles.append(angle)
 
-            if all(a >= 85 and a <= 95 for a in angles):
-              x, y, w, h = cv.boundingRect(cnt)
-              return img[y:y+h, x:x+w]
+          if all(a >= 85 and a <= 95 for a in angles):
+            x, y, w, h = cv.boundingRect(cnt)
+            return img[y:y+h, x:x+w]
   return None
--- processtest.py
+++ processtest.py
@@ -1,11 +1,30 @@
 import cv2 as cv
 import imgprocess
 
-img = cv.imread('img/multimetro.jpg')
-img = cv.resize(img, (500, 500))
-device = imgprocess.getDeviceContour(img)
-display = imgprocess.getDisplayContour(device, 197 * 62)
+imgs = [
+  cv.imread('img/multimetro.jpg'),
+  cv.imread('img/multimetro1.jpg'),
+  cv.imread('img/multimetro2.jpg'),
+  cv.imread('img/multimetro3.jpg'),
+  cv.imread('img/multimetro4.jpg'),
+  cv.imread('img/multimetro5.jpg'),
+  cv.imread('img/multimetro6.jpg'),
+  cv.imread('img/multimetro7.jpg'),
+]
 
-cv.imshow('Device', device)
-cv.imshow('Display', display)
-cv.waitKey(0)
+for i in range(len(imgs)):
+  img = cv.resize(imgs[i], (500, 500))
+  device, device_w, device_h = imgprocess.getDeviceContour(img)
+  binary_img = imgprocess.getBinaryImage(img, (3, 3))
+
+  cv.imshow('Image', img)
+  cv.imshow('Binary', binary_img)
+  if device is not None:
+    cv.imshow('Device', device)
+    display = imgprocess.getDisplayContour(device, (device_w * device_h) * 0.15)
+    if display is not None:
+      cv.imshow('Display', display)
+  cv.waitKey(0)
+  cv.destroyAllWindows()
+# cv.imshow('Device', device)
+# cv.imshow('Display', display)
